a320bc4577c8b00ed35d2bdc8f57f755d0cd1494
raymondhan <raymondhan@foxmail.com>
2022-03-27 16:56:17 +0800

    72

---
+++ leetcode72.py
@@ -0,0 +1,18 @@
+class Solution:
+    @classmethod
+    def minDistance(cls, word1: str, word2: str) -> int:
+        l1, l2 = len(word1), len(word2)
+        dp = [[0 for i in range(l2+1)] for j in range(l1+1)]
+        for i in range(l1+1):
+            dp[i][0] = i
+        for i in range(l2+1):
+            dp[0][i] = i
+        for i in range(1,l1+1):
+            for j in range(1,l2+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]))+1
+        return dp[l1][l2]
+
+print(Solution.minDistance("intention","execution"))
